304e2df30022d9c96150181ccb6e7b7b78e8603d
Ruud Greven <r.greven@saxion.nl>
2017-01-10 11:32:29 +0100

    Updated documentation and bower.json

--- website/README.md
+++ website/README.md
@@ -4,7 +4,8 @@ This project is generated with [yo angular generator](https://github.com/yeoman/
 version 0.15.1.
 
 ## Build & development
-
+Run `npm install` to install Grunt and dependencies
+Run `bower install` to install Bower components
 Run `grunt` for building and `grunt serve` for preview.
 
 ## Testing
--- website/test/karma.conf.js
+++ website/test/karma.conf.js
@@ -31,6 +31,10 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-material/angular-material.js',
       'bower_components/angular-material-data-table/dist/md-data-table.js',
+      'bower_components/d3/d3.js',
+      'bower_components/vega/vega.js',
+      'bower_components/vega-lite/vega-lite.js',
+      'bower_components/viscompass/compass.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
